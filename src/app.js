a0d964f6fb745fec867dc92cf750ab760dd0bc9f
Yash Sathe <yashsathe1357@gmail.com>
2026-08-19 19:34:12 +0530

    Utils code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//Created a app
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//configuration
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+//exported the  app
+export { app };
