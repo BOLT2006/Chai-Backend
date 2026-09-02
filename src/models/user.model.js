5b4ff7a75516016215cc08092a1bac160af9cb82
Yash Sathe <yashsathe1357@gmail.com>
2026-09-02 21:51:12 +0530

    add a register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 //prehook
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10); // encrypt the password
+  this.password = await bcrypt.hash(this.password, 10); // encrypt the password
   next();
 });
 
